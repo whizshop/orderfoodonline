ad2e003fdc8dc64956e77e6042295e821be79539
WHIZ KEITIE <whizhostke@gmail.com>
2025-02-16 23:36:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-// Sample data for food items
-let foodItems = [
-    { name: "Burger - Kes 600", img: "https://i.pinimg.com/736x/eb/cb/c6/ebcbc6aaa9deca9d6efc1efc93b66945.jpg" },
-    { name: "Pasta - Kes 750", img: "https://i.pinimg.com/736x/91/f4/75/91f475ea479986f068cc8a9fd7517bca.jpg" },
-    { name: "Sushi - Kes 670", img: "https://i.pinimg.com/736x/ad/10/24/ad1024bec8ffd60319b5157195847ba5.jpg" },
-    { name: "Tacos - Kes 2100", img: "https://i.pinimg.com/736x/db/c4/c0/dbc4c003d5674030efe2173d2b8dcbce.jpg" },
-    { name: "Salad - Kes 350", img: "https://i.pinimg.com/736x/45/73/d9/4573d97e97a507d2eadbe34261ad0b62.jpg" }
+// Food Items
+const foodItems = [
+    { name: "Pizza Kes 10000", img: "https://i.pinimg.com/736x/1e/a7/fc/1ea7fca510708ed25f97b2ca63809d32.jpg" },
+    { name: "Burger Kes 600", img: "https://i.pinimg.com/736x/eb/cb/c6/ebcbc6aaa9deca9d6efc1efc93b66945.jpg" },
+    { name: "Pasta Kes 750", img: "https://i.pinimg.com/736x/91/f4/75/91f475ea479986f068cc8a9fd7517bca.jpg" },
+    { name: "Sushi Kes 670", img: "https://i.pinimg.com/736x/ad/10/24/ad1024bec8ffd60319b5157195847ba5.jpg" },
+    { name: "Salad Kes 350", img: "https://i.pinimg.com/736x/45/73/d9/4573d97e97a507d2eadbe34261ad0b62.jpg" },
+    { name: "Tacos Kes 2100", img: "https://i.pinimg.com/736x/db/c4/c0/dbc4c003d5674030efe2173d2b8dcbce.jpg" }
 ];
-
-// Shuffle images before displaying
-function shuffleFoodItems() {
-    foodItems = foodItems.sort(() => Math.random() - 0.5);
-}
 
 // Display Food Items
 function displayFoodItems() {
-    shuffleFoodItems();
     const foodContainer = document.getElementById('food-items');
     foodContainer.innerHTML = '';
 
@@ -30,24 +25,21 @@ function displayFoodItems() {
     });
 }
 
-// Cart functionality
+// Cart
 let cart = [];
 function addToCart(itemName) {
     cart.push(itemName);
-    alert(`${itemName} has been added to your cart!`);
-    updateCartUI();
+    document.getElementById('cart-count').innerText = cart.length;
 }
 
-function updateCartUI() {
-    const cartList = document.getElementById('cart-items');
-    cartList.innerHTML = cart.map(item => `<li>${item}</li>`).join('');
+// Popups
+function openPopup(id) {
+    document.getElementById(id).style.display = "flex";
 }
 
-// Search filter
-function filterMenu() {
-    let searchInput = document.getElementById('search').value.toLowerCase();
-    displayFoodItems(foodItems.filter(item => item.name.toLowerCase().includes(searchInput)));
+function closePopup(id) {
+    document.getElementById(id).style.display = "none";
 }
 
-// Load menu on page load
+// Load
 displayFoodItems();
